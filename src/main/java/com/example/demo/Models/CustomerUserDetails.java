c2b8f9397c5a935fb27656671fdfb5d3185710a6
toiluoicode <104342280+toiluoicode@users.noreply.github.com>
2024-05-06 16:14:00 +0700

    register

--- src/main/java/com/example/demo/Models/CustomerUserDetails.java
+++ src/main/java/com/example/demo/Models/CustomerUserDetails.java
@@ -4,7 +4,6 @@ import com.example.demo.Repository.UserRepository;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
